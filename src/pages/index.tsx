29b6c903df76c99034588474cda8d573cd1373c0
Mahesh Samudra <maheshsamudra@gmail.com>
2023-07-04 15:35:32 +0530

    WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Layout from "@/components/Layout";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Layout metadata={{ title: "Welcome" }}>Home Page</Layout>;
+  return (
+    <Layout metadata={{ title: "Welcome" }}>
+      Home Page
+      <Button variant="outline">Button</Button>
+    </Layout>
+  );
 }
