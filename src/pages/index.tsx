5c354ed53016483207cab06d381477395eb1610a
Mahesh Samudra <maheshsamudra@gmail.com>
2023-07-05 08:41:09 +0530

    Images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,12 @@ export default function Home() {
     <Layout metadata={{ title: "Welcome" }}>
       Home Page
       <Button variant="outline">Button</Button>
+      <img
+        // src="https://placehold.co/1600x900"
+        srcSet="https://placehold.co/160x90 160w, https://placehold.co/320x180 320w, https://placehold.co/800x450 800w, https://placehold.co/1600x900 1600w"
+        alt=""
+        style={{ width: "100%" }}
+      />
     </Layout>
   );
 }
